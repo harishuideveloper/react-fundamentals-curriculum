f229a84f173b623ec8219df4fe1ff599dcbb194c
Harish Mohan Raj <hraj@sapient.com>
2017-05-11 07:27:33 +0100

    files committed

--- app/components/DayContainer.js
+++ app/components/DayContainer.js
@@ -1,10 +1,8 @@
 var React = require('react');
-var NavBar = require('./NavBar');
 
 const DayContainer = (props) => {
     return(
         <div className='container'>
-            <NavBar />
             <div onClick={props.onClick} className='dayContainer'>
                 <img 
                     src={'/app/images/weather-icons/' + props.icon + '.svg'}
